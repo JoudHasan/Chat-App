6e1dd79e3d2ad8366d6c1fade091393ea44c6cdc
Joud <joud.hasan85@gmail.com>
2024-07-03 17:31:23 +0200

    new

--- App.js
+++ App.js
@@ -7,33 +7,38 @@ import {
   disableNetwork,
   enableNetwork,
 } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getStorage } from "firebase/storage";
-import Start from "./components/Start";
-import Chat from "./components/Chat";
 import { useNetInfo } from "@react-native-community/netinfo";
 import { Alert } from "react-native";
+import Start from "./components/Start";
+import Chat from "./components/Chat";
+
+const Stack = createNativeStackNavigator();
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEN5zRsJ6dzFFpiqRuW9RLA5VIpL8WbaQ",
+  authDomain: "chat-app-26204.firebaseapp.com",
+  projectId: "chat-app-26204",
+  storageBucket: "chat-app-26204.appspot.com",
+  messagingSenderId: "321927240395",
+  appId: "1:321927240395:web:7b6b003d7a93b7f101d94e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+const db = getFirestore(app);
+const storage = getStorage(app);
 
 // Main App component
 const App = () => {
-  const Stack = createNativeStackNavigator();
   const connectionStatus = useNetInfo();
 
-  // Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAEN5zRsJ6dzFFpiqRuW9RLA5VIpL8WbaQ",
-    authDomain: "chat-app-26204.firebaseapp.com",
-    projectId: "chat-app-26204",
-    storageBucket: "chat-app-26204.appspot.com",
-    messagingSenderId: "321927240395",
-    appId: "1:321927240395:web:7b6b003d7a93b7f101d94e",
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  const storage = getStorage(app);
-
-  // Network connectivity status
   useEffect(() => {
     if (connectionStatus.isConnected === false) {
       Alert.alert("Connection lost");
--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,79 +1,179 @@
-import React from "react";
-import { TouchableOpacity, StyleSheet, View, Text, Alert } from "react-native";
-import * as Location from "expo-location";
+import React, { useState, useEffect } from "react";
+import {
+  TouchableOpacity,
+  View,
+  Alert,
+  StyleSheet,
+  Modal,
+  Text,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { Ionicons } from "@expo/vector-icons";
+import * as Location from "expo-location";
+import { Audio } from "expo-av";
+import { uploadBytes, ref, getDownloadURL } from "firebase/storage";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage }) => {
-  // Pick an image from the library
+const CustomActions = ({
+  wrapperStyle,
+  iconTextStyle,
+  onSend,
+  storage,
+  id,
+}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [recordingObject, setRecordingObject] = useState(null);
+
+  useEffect(() => {
+    return () => {
+      if (recordingObject) {
+        recordingObject.stopAndUnloadAsync();
+      }
+    };
+  }, [recordingObject]);
+
   const pickImage = async () => {
-    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (permissions.granted) {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      });
-      if (!result.canceled) {
-        const imageURI = result.assets[0].uri;
-        await uploadAndSendImage(imageURI); // Upload and send the chosen image
-      }
-    } else {
+    setModalVisible(false);
+    const permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!permissions.granted) {
       Alert.alert("Permissions haven't been granted.");
+      return;
     }
-  };
 
-  // Take a photo using the camera
-  const takePhoto = async () => {
-    let permissions = await ImagePicker.requestCameraPermissionsAsync();
-    if (permissions.granted) {
-      let result = await ImagePicker.launchCameraAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      });
-      if (!result.canceled) {
-        const imageURI = result.assets[0].uri;
-        await uploadAndSendImage(imageURI); // Upload and send the taken photo
-      }
-    } else {
-      Alert.alert("Permissions haven't been granted.");
-    }
-  };
-
-  // Upload and send an image to Firestore
-  const uploadAndSendImage = async (imageURI) => {
-    const uniqueRefString = generateReference(imageURI);
-    const newUploadRef = ref(storage, uniqueRefString);
-    const response = await fetch(imageURI);
-    const blob = await response.blob();
-    uploadBytes(newUploadRef, blob).then(async (snapshot) => {
-      const imageURL = await getDownloadURL(snapshot.ref);
-      onSend({ image: imageURL }); // Send the image URL in the chat
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
     });
+
+    if (!result.canceled) {
+      const imageURI = result.assets[0].uri;
+      console.log("Image URI:", imageURI); // Logging the image URI
+      await uploadAndSendImage(imageURI);
+    }
   };
 
-  // Generate a unique reference for the image
+  const takePhoto = async () => {
+    setModalVisible(false);
+    const permissions = await ImagePicker.requestCameraPermissionsAsync();
+    if (!permissions.granted) {
+      Alert.alert("Permissions haven't been granted.");
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    });
+
+    if (!result.canceled) {
+      const imageURI = result.assets[0].uri;
+      console.log("Photo URI:", imageURI); // Logging the photo URI
+      await uploadAndSendImage(imageURI);
+    }
+  };
+
+  const uploadAndSendImage = async (imageURI) => {
+    try {
+      const uniqueRefString = generateReference(imageURI);
+      const newUploadRef = ref(storage, uniqueRefString);
+      const response = await fetch(imageURI);
+      const blob = await response.blob();
+      console.log("Uploading image...");
+      uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+        const imageURL = await getDownloadURL(snapshot.ref);
+        console.log("Image URL:", imageURL); // Logging the image URL
+        onSend({ image: imageURL });
+      });
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  };
+
   const generateReference = (uri) => {
     const timeStamp = new Date().getTime();
     const imageName = uri.split("/").pop();
-    return `${timeStamp}-${imageName}`;
+    return `${id}/${timeStamp}-${imageName}`;
   };
 
-  // Get the current location of the user
   const getLocation = async () => {
-    let permissions = await Location.requestForegroundPermissionsAsync();
-    if (permissions.granted) {
-      const location = await Location.getCurrentPositionAsync({});
-      if (location) {
-        onSend({
-          location: {
-            longitude: location.coords.longitude,
-            latitude: location.coords.latitude,
-          },
-        }); // Send the location in the chat
-      } else {
-        Alert.alert("Error occurred while fetching location");
-      }
-    } else {
+    setModalVisible(false);
+    const permissions = await Location.requestForegroundPermissionsAsync();
+    if (!permissions.granted) {
       Alert.alert("Permissions haven't been granted.");
+      return;
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+    if (location) {
+      onSend({
+        location: {
+          longitude: location.coords.longitude,
+          latitude: location.coords.latitude,
+        },
+      });
+    } else {
+      Alert.alert("Error occurred while fetching location");
+    }
+  };
+
+  const startRecording = async () => {
+    setModalVisible(false);
+    const permissions = await Audio.requestPermissionsAsync();
+    if (!permissions.granted) {
+      Alert.alert("Failed to get recording permissions.");
+      return;
+    }
+
+    try {
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecordingObject(recording);
+
+      Alert.alert(
+        "Recording...",
+        undefined,
+        [
+          {
+            text: "Cancel",
+            onPress: stopRecording,
+          },
+          {
+            text: "Stop and Send",
+            onPress: sendRecordedSound,
+          },
+        ],
+        { cancelable: false }
+      );
+    } catch (error) {
+      Alert.alert("Failed to start recording!");
+    }
+  };
+
+  const stopRecording = async () => {
+    if (recordingObject) {
+      await recordingObject.stopAndUnloadAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+        playsInSilentModeIOS: false,
+      });
+    }
+  };
+
+  const sendRecordedSound = async () => {
+    if (recordingObject) {
+      await stopRecording();
+      const uri = recordingObject.getURI();
+      const uniqueRefString = generateReference(uri);
+      const newUploadRef = ref(storage, uniqueRefString);
+      const response = await fetch(uri);
+      const blob = await response.blob();
+      uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+        const soundURL = await getDownloadURL(snapshot.ref);
+        onSend({ audio: soundURL });
+      });
     }
   };
 
@@ -81,41 +181,71 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage }) => {
     <View style={styles.mainContainer}>
       <TouchableOpacity
         style={[styles.container, wrapperStyle]}
-        onPress={pickImage}
+        onPress={() => setModalVisible(true)}
         accessible={true}
-        accessibilityLabel="Choose an image from the library"
-        accessibilityHint="Open the media library to choose an image"
+        accessibilityLabel="Show actions"
+        accessibilityHint="Show action sheet to choose an action"
         accessibilityRole="button"
       >
-        <Ionicons name="image-outline" size={24} color="white" />
+        <Ionicons name="add-circle-outline" size={24} color="white" />
       </TouchableOpacity>
 
-      <TouchableOpacity
-        style={[styles.container, wrapperStyle]}
-        onPress={takePhoto}
-        accessible={true}
-        accessibilityLabel="Take a picture"
-        accessibilityHint="Open the camera to take a picture"
-        accessibilityRole="button"
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
       >
-        <Ionicons name="camera-outline" size={24} color="white" />
-      </TouchableOpacity>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <TouchableOpacity
+              style={[styles.modalButton, wrapperStyle]}
+              onPress={pickImage}
+            >
+              <Ionicons name="image-outline" size={24} color="white" />
+              <Text style={styles.buttonText}>Choose From Library</Text>
+            </TouchableOpacity>
 
-      <TouchableOpacity
-        style={[styles.container, wrapperStyle]}
-        onPress={getLocation}
-        accessible={true}
-        accessibilityLabel="Send location"
-        accessibilityHint="Send your current location"
-        accessibilityRole="button"
-      >
-        <Ionicons name="location-outline" size={24} color="white" />
-      </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.modalButton, wrapperStyle]}
+              onPress={takePhoto}
+            >
+              <Ionicons name="camera-outline" size={24} color="white" />
+              <Text style={styles.buttonText}>Take Picture</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[styles.modalButton, wrapperStyle]}
+              onPress={getLocation}
+            >
+              <Ionicons name="location-outline" size={24} color="white" />
+              <Text style={styles.buttonText}>Send Location</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[styles.modalButton, wrapperStyle]}
+              onPress={startRecording}
+            >
+              <Ionicons name="mic-outline" size={24} color="white" />
+              <Text style={styles.buttonText}>Record Sound</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[styles.modalButton, wrapperStyle]}
+              onPress={() => setModalVisible(false)}
+            >
+              <Ionicons name="close-outline" size={24} color="white" />
+              <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
-// Define styles for the CustomActions component
 const styles = StyleSheet.create({
   mainContainer: {
     flexDirection: "row",
@@ -128,6 +258,31 @@ const styles = StyleSheet.create({
     backgroundColor: "#075E54",
     borderRadius: 25,
     marginHorizontal: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalContent: {
+    width: 300,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 20,
+    alignItems: "center",
+  },
+  modalButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: "#075E54",
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: "white",
+    marginLeft: 10,
   },
 });
 
